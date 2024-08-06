d5864b402203c7fcfcc6bfeda992fd141d60267e
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-08-06 19:48:47 -0300

    Encapsulamento, Métodos Getters e Setters por referência

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,11 +1,48 @@
 
 public class Cliente {
 
-    String nome;
-    String cpf;
-    String profissao;
-    double salario;
+    private String nome;
+    private String cpf;
+    private  String profissao;
+    private double salario;
 
+    // USANDO MÉTODO GETTERS E SETTERS
+    public String getNome(){
+        return this.nome;
+    }
 
+    //SETTER
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 
+    // GETTER
+    public String getCpf() {
+        return cpf;
+    }
+
+    //SETTER
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    // GETTER
+    public String getProfissao() {
+        return profissao;
+    }
+
+    //SETTER
+    public void setProfissao(String profissão) {
+        this.profissao = profissão;
+    }
+
+    // GETTER
+    public double getSalario() {
+        return salario;
+    }
+
+    //SETTER
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
--- src/TesteEncapsulamento.java
+++ src/TesteEncapsulamento.java
@@ -1,16 +1,35 @@
 public class TesteEncapsulamento {
     public static void main(String[] args) {
+        // criando cliente
+        Cliente clienteIcaro = new Cliente();
+        clienteIcaro.setNome("Ícaro");
+        clienteIcaro.setProfissao("Programador");
+        clienteIcaro.setCpf("18877429720");
+        clienteIcaro.setSalario(25000);
 
         //Testando encapsulamento
         Conta conta = new Conta();
-
+        conta.setAgencia(10);// adicionando um valor a agencia com Set
         conta.depositar(1000);
-        conta.sacar(1000);
-
-        // MÉTODO GETTERS para pegar/retornar o valor do saldo
+        conta.setTitular(clienteIcaro);
+        // MÉTODO GETTERS para retornar o valor do saldo
         System.out.println(conta.getSaldo());
+        // pegando o titular através do método get
+        System.out.println(conta.getTitular().getNome());
+        System.out.println("saldo: " + conta.getSaldo());
+        // testando se esta no mesmo espaço de memoria se é o mesmo cliente mesmo
+        System.out.println("mesmo caminho: " + clienteIcaro);
+        System.out.println("mesmo caminho: " +conta.getTitular());
 
+        // criando um novo titular
+        Conta contaTwo = new Conta();
 
+        contaTwo.setTitular(new Cliente());
+        contaTwo.getTitular().setNome("Pedro");
+        contaTwo.getTitular().setSalario(10000);
+
+        System.out.println("DOIS: "+ contaTwo.getTitular().getNome());
+        System.out.println("DOIS: "+ contaTwo.getTitular().getSalario());
 
     }
 }
